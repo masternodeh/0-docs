0b443ce3544645999878231f14d818c3ee6c0670
Document Daemon <hello@masternode.io>
2021-05-13 14:33:20 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     GUIDES: ['eth', 'avax', 'bsc','sol','mobile'],
     EXPLAINERS: ['grow','il','nft','rugpull','apes'],
 	FAQ: ['faq','faq001','faq002','faq003','faq004','faq005','faq006','faq007','faq008','faq009','faq010','faq011','faq012','faq013','faq014','faq015','faq016','faq017','faq018'],
-	WIKI: ['howToBuyZero','roadmap','partnerships','zeroGravity','fructifyZero','descZeroExchanage','communities'],
+	WIKI: ['howToBuyZero','roadmap','partnerships','zeroGravity','fructifyZero','descZeroExchange','communities'],
   },
 };
