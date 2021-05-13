ffc723f5c81f56d44ff4fde63c6dbd881cf044e0
Document Daemon <hello@masternode.io>
2021-05-13 14:28:33 -0700

    Sidebar adjustments

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     Guides: ['eth', 'avax', 'bsc','sol','mobile'],
     Explainers: ['grow','il','nft','rugpull','apes'],
 	FAQ: ['faq','faq001','faq002','faq003','faq004','faq005','faq006','faq007','faq008','faq009','faq010','faq011','faq012','faq013','faq014','faq015','faq016','faq017','faq018']
-	Wiki: ['howToBuyZero','roadmap','partnerships','zeroGravity','fructifyZero','description','communities'],
+	Wiki: ['howtobuyzero','roadmap','partnerships','zerogravity','fructifyZero','description','communities'],
   },
 };
