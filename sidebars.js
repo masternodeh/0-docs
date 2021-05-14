12023fbd5f241c686de38c6f807700636c8d3d3a
Document Daemon <hello@masternode.io>
2021-05-13 21:10:29 -0700

    Sidebar Reorg

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
-    GUIDES: ['eth', 'avax', 'bsc','sol','mobile'],
-    EXPLAINERS: ['grow','il','nft','rugpull','apes'],
-	FAQ: ['faq','faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018'],
-	WIKI: ['wiki/howtobuyzero','wiki/roadmap','wiki/partnerships','wiki/zerogravity','wiki/fructifyzero','wiki/whatiszeroexchange','wiki/communities'],
+    GUIDES: ['whatiszeroexchange','howtobuyzero','communities','zerogravity','eth', 'avax', 'bsc','roadmap','partnerships','fructifyzero','mobile'],
+	FAQ: ['faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018'],
+	EXPLAINERS: ['il','nft','rugpull'],
+	FUTURE: ['sol','grow','apes'],
   },
 };
