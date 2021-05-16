b86ead753d2c15439cd8ac41f489cf69d277212c
JohnDollarZero <84021567+JohnDollarZero@users.noreply.github.com>
2021-05-16 10:55:23 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    GUIDES: ['whatiszeroexchange','howtobuyzero','zerogravity','fructifyzero','roadmap','partnerships','communities','mobile','eth', 'avax', 'bsc'],
+    GUIDES: ['whatiszeroexchange','howtobuyzero','zerogravity','fructifyzero','roadmap','partnerships','communities'],
+	GUIDES archives: ['mobile','eth', 'avax', 'bsc'],	  
 	FAQ: ['faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018'],
 	IDO: ['IDO/ido001','IDO/ido002','IDO/ido003','IDO/ido004'],
 	EXPLAINERS: ['il','nft','rugpull'],
