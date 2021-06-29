36f8e2df124fb8ae231064a335b3f25727ac90d7
Document Daemon <hello@masternode.io>
2021-06-29 07:24:39 -0700

    Added osmosis page and proposal page for ion proposals

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,11 @@
 module.exports = {
   someSidebar: {
-    GUIDES: ['whatiszeroexchange','howtobuyzero','zerobridge','zerogravity','fructifyzero','roadmap','partnerships','defiacademy','faq', 'communities','mobile','eth','avax','bsc','polygon'],	  
+    GUIDES: ['whatiszeroexchange','howtobuyzero','zerobridge','zerogravity','fructifyzero','roadmap','partnerships','defiacademy','faq', 'communities','mobile'],	  
 	FAQ: ['faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018','faq/faq019','faq/faq020','faq/faq021','faq/faq022','faq/faq023','faq/faq024','faq/faq025','faq/faq026','faq/faq027','faq/faq028'],
 	IDO: ['ido/ido001','ido/ido002','ido/ido003','ido/ido004','ido/ido005'],
 	HELPDESK: ['sd/sd001'], 
-	RELAY: ['relay/relaylaunch'],  
+	RELAY: ['relay/relaylaunch'],
+    BLOCKCHAINS: ['eth','avax','bsc','polygon','osmosis','osmosis/ion/prop001'],
 	EXPLAINERS: ['il','nft','rugpull'],
 	FUTURE: ['sol','grow','apes'],
   },
