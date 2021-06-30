6347c8caac02730d0c5b77b73c6a782ad3056078
JohnDollarZero <84021567+JohnDollarZero@users.noreply.github.com>
2021-06-30 16:31:10 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     FAQ: ['faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018','faq/faq019','faq/faq020','faq/faq021','faq/faq022','faq/faq023','faq/faq024','faq/faq025','faq/faq026','faq/faq027','faq/faq028'],
     IDO: ['ido/ido001','ido/ido002','ido/ido003','ido/ido004','ido/ido005'],
     HELPDESK: ['sd/sd001'], 
-    RELAY: ['relay/relaylaunch', 'relay/lp-ZERO-2-RELAY'],
+    RELAY: ['relay/relaylaunch', 'relay/lpzero2relay'],
     BLOCKCHAINS: ['eth','avax','bsc','polygon','sol','osmosis'],
     PROPOSALS: ['osmosis/ion/whatision','osmosis/ion/prop001','osmosis/ion/prop002'],
     EXPLAINERS: ['il','nft','rugpull'],
